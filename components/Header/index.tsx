0a5c079ea5d009b733444bc5cfe26f23d23fc6ee
jinxudong <18751241086@163.com>
2024-09-11 00:45:45 +0800

    导航路由完善

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import type { NextPage } from "next";
 import styles from './index.module.scss'
 import {navs} from './config'
 import Link from "next/link";
+import { usePathname, useSearchParams } from 'next/navigation';
 
 const Header = () => {
+  console.log('searchParams.get("id")');
+  const pathName = usePathname(); // 获取当前路径
+  const searchParams = useSearchParams(); // 获取查询参数
+  console.log('pathName',pathName);
+
   return (
     <div className={styles.navbar}>
       <section className={styles.logArea}>BLOG-C</section>
@@ -11,7 +19,12 @@ const Header = () => {
         {
           navs.map(item => (
             <Link href={item.value} key={item.value}>
-              <div className={styles.navItem}>{item.label}</div>
+              {/* <div className={styles.navItem}>{item.label}</div> */}
+              <div
+                className={`${styles.navItem} ${pathName === item.value ? styles.active : ''}`}
+              >
+                {item.label}
+            </div>
             </Link>
           ))
         }
